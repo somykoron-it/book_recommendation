bfad8cd3c76da980f9be5c19d6ee8687f04a9297
Nur <cocshishir@gmail.com>
2025-10-17 18:22:53 +0600

    Basic UI update

--- app/page.js
+++ app/page.js
@@ -1,37 +1,12 @@
-import Navbar from "./components/LandingPage/Navbar";
-import HeroSection from "./components/LandingPage/HeroSection";
-import TrustIndicators from "./components/LandingPage/TrustIndicators";
-import Features from "./components/LandingPage/Features";
-import Testimonials from "./components/LandingPage/Testimonials";
-import CTA from "./components/LandingPage/CTA";
-import Footer from "./components/LandingPage/Footer";
 import PublicRoute from "./components/context.js/PublicRoute";
+import HeroSection from "./components/LandingPage/HeroSection";
 
 export default function LandingPage() {
   return (
     <PublicRoute>
-      {" "}
-      <div className="min-h-screen flex flex-col bg-white">
-        {/* Navbar */}
-        <Navbar />
-
+      <div className="flex flex-col bg-white">
         {/* Hero Section */}
         <HeroSection />
-
-        {/* Trust Indicators */}
-        <TrustIndicators />
-
-        {/* Features Section */}
-        <Features />
-
-        {/* Testimonials */}
-        <Testimonials />
-
-        {/* Final CTA */}
-        <CTA />
-
-        {/* Footer */}
-        <Footer />
       </div>
     </PublicRoute>
   );
